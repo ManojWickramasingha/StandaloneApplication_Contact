3467bd7a73cbe7ac49dd90fb6602f0a33989bb08
Manoj wickramasingha <141001887+ManojWickramasingha@users.noreply.github.com>
2024-06-07 14:03:00 +0530

    Add Create new Contact

--- Ifrend.java
+++ Ifrend.java
@@ -3,6 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.table.*;
+import javax.swing.JOptionPane;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 
 class Contact {
     private String contactID;
@@ -72,95 +76,376 @@ class Contact {
     }
 }
 
-class ContactHomeForm extends JFrame {
+// -----------------------Add New Contact Form---------------------
 
-    JLabel homelabel;
-    JButton addnewcontact;
-    JButton updatecontact;
-    JButton searchcontact;
-    JButton deletecontact;
-    JButton viewcontact;
-    JButton exit;
+class AddContactForm extends JFrame {
+    private JLabel titleLabel;
 
-    ContactHomeForm() {
-        setSize(500, 600);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLocationRelativeTo(null);
-        JPanel ContainParentPanner = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    private JButton btnAdd;
+    private JButton btnCancel;
+    private JButton btnBack;
 
-        JPanel ContentPannel = new JPanel(new GridLayout(7, 1));
+    private JLabel lblName;
+    private JLabel lblContactNumber;
+    private JLabel lblCompany;
+    private JLabel lblSalary;
+    private JLabel lblBirthday;
 
-        JPanel homeContactpannel = new JPanel(new FlowLayout());
+    private JTextField textName;
+    private JTextField textContactNumber;
+    private JTextField textCompany;
+    private JTextField textSalary;
+    private JTextField textBirthday;
+    JTextField id;
 
-        homelabel = new JLabel("Home Page");
-        homelabel.setFont(new Font("", 1, 30));
+    public static String GenarateContactID(int count) {
+        int[] temp = new int[4];
+        int j = temp.length - 1;
+        String s = "C";
+        while (count != 0) {
+            int rem = count % 10;
+            if (j >= 0) {
+                temp[j] = rem;
+                j--;
+            } else {
+                System.out.print("	memory full!");
+                return s;
+            }
+            count /= 10;
 
-        homeContactpannel.add(homelabel);
-        ContentPannel.add(homeContactpannel);
+        }
+        for (int i = 0; i < temp.length; i++) {
+            s += temp[i];
+        }
 
-        JPanel addNewContact = new JPanel(new FlowLayout());
-
-        addnewcontact = new JButton("Add New Contact");
-        addnewcontact.setFont(new Font("", 1, 20));
-        addnewcontact.setHorizontalAlignment(JButton.CENTER);
-
-        addNewContact.add(addnewcontact);
-        ContentPannel.add(addNewContact);
-
-        JPanel updatecontactPannel = new JPanel(new FlowLayout());
-
-        updatecontact = new JButton("Update Contact");
-        updatecontact.setFont(new Font("", 1, 20));
-        updatecontact.setHorizontalAlignment(JButton.CENTER);
-
-        updatecontactPannel.add(updatecontact);
-        ContentPannel.add(updatecontactPannel);
-
-        JPanel searchcontactPannel = new JPanel(new FlowLayout());
-
-        searchcontact = new JButton("Search Contact");
-        searchcontact.setFont(new Font("", 1, 20));
-        searchcontact.setHorizontalAlignment(JButton.CENTER);
-
-        searchcontactPannel.add(searchcontact);
-        ContentPannel.add(searchcontactPannel);
-
-        JPanel deletecontactPannel = new JPanel(new FlowLayout());
-
-        deletecontact = new JButton("Delete Contact");
-        deletecontact.setFont(new Font("", 1, 20));
-        deletecontact.setHorizontalAlignment(JButton.CENTER);
-
-        deletecontactPannel.add(deletecontact);
-        ContentPannel.add(deletecontactPannel);
-
-        JPanel viewcontactPannel = new JPanel(new FlowLayout());
-
-        viewcontact = new JButton("View Contact");
-        viewcontact.setFont(new Font("", 1, 20));
-        viewcontact.setHorizontalAlignment(JButton.CENTER);
-
-        viewcontactPannel.add(viewcontact);
-        ContentPannel.add(viewcontactPannel);
-
-        JPanel exitPannel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
-        exit = new JButton("Exit");
-        exit.setFont(new Font("", 1, 20));
-        // exit.setHorizontalAlignment(JButton.CENTE);
-        exitPannel.add(exit);
-        ContentPannel.add(exitPannel);
-
-        ContainParentPanner.add(ContentPannel);
-
-        add("North", ContentPannel);
+        return s;
     }
 
+    public static boolean isduplicate(String ID) {
+
+        int length = ContactHomeForm.contactList.size();
+        for (int i = 0; i < length; i++) {
+            if (ID.equals(ContactHomeForm.contactList.get(i).getContactID())
+                    || ID.equals(ContactHomeForm.contactList.get(i).getContactID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // static int count = 0;
+    public static String displayID(int count) {
+
+        String ID = GenarateContactID(++count);
+        System.out.println(" " + ID);
+        System.out.println(" ======");
+        String check = "C";
+
+        if (check.equals(ID)) {
+            System.out.println("\n\n");
+            return "Full contact Number!";
+        }
+
+        if (isduplicate(ID)) {
+            return "Duplicate";
+        }
+
+        return ID;
+    }
+
+    // ----validate contact number------
+    public static boolean validateNumber(String PhoneNumber) {
+        char[] original = new char[0];
+        int count = 0;
+        for (int i = 0; i < PhoneNumber.length(); i++) {
+            char[] temp = new char[original.length + 1];
+            // ---------------copy to data--------------
+            for (int j = 0; j < original.length; j++) {
+                temp[j] = original[j];
+            }
+            if (PhoneNumber.charAt(0) != '0') {
+                return false;
+            } else {
+                original = temp;
+                original[original.length - 1] = PhoneNumber.charAt(i);
+                count++;
+            }
+        }
+        return count == 10;
+
+    }
+
+    public static boolean validateBDay(String BDay) {
+        SimpleDateFormat sam = new SimpleDateFormat("YYY-MM-DD");
+        String nowDate = sam.format(new Date());
+        try {
+            Date d1 = sam.parse(BDay);
+            Date d2 = sam.parse(nowDate);
+            if (d2.before(d1)) {
+                return false;
+            }
+        } catch (ParseException e) {
+
+        }
+
+        return true;
+    }
+
+    AddContactForm(int count) {
+        setSize(500, 300);
+        setTitle("Add Contact Form");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        titleLabel = new JLabel("Add Contact");
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setFont(new Font("", 1, 30));
+        add("North", titleLabel);
+
+        JPanel btnGrid = new JPanel(new GridLayout(2, 1));
+
+        JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        btnAdd = new JButton("ADD Contact");
+        btnAdd.setFont(new Font("", 1, 20));
+        btnAdd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                String Id = id.getText();
+                String name = textName.getText();
+                String contactnumber = textContactNumber.getText();
+                if (validateNumber(contactnumber)) {
+                    if (!isduplicate(contactnumber)) {
+                        String company = textCompany.getText();
+                        double salary = Double.parseDouble(textSalary.getText());
+                        if (salary > 0) {
+                            String birthday = textBirthday.getText();
+                            if (validateBDay(birthday)) {
+                                Contact contact = new Contact(Id, name, contactnumber, company, salary, birthday);
+                                ContactHomeForm.contactList.add(contact);
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Not a Validate Birthday",
+                                        "agin added the new Birthday",
+                                        JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Not a Negative Salary", "agin added the new Salary",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        }
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Duplicate Contact Number", "agin added the new number",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "not valide Contact Number", "agin added the new number",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+
+            }
+        });
+        btnPanel.add(btnAdd);
+
+        btnCancel = new JButton("Cancel");
+        btnCancel.setFont(new Font("", 1, 20));
+        btnPanel.add(btnCancel);
+
+        JPanel backPannel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        btnBack = new JButton("Back   To   Home  Page");
+        btnBack.setFont(new Font("", 1, 20));
+        backPannel.add(btnBack);
+        btnPanel.add(backPannel);
+
+        btnGrid.add(btnPanel);
+        btnGrid.add(backPannel);
+
+        add("South", btnGrid);
+
+        // JPanel lblandtextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JPanel lblPannel = new JPanel(new GridLayout(6, 1));
+
+        JLabel ID = new JLabel("Contact ID");
+        ID.setFont(new Font("", 1, 20));
+        lblPannel.add(ID);
+
+        lblName = new JLabel("Name");
+        lblName.setFont(new Font("", 1, 20));
+        lblPannel.add(lblName);
+
+        lblContactNumber = new JLabel("Contact Number");
+        lblContactNumber.setFont(new Font("", 1, 20));
+        lblPannel.add(lblContactNumber);
+
+        lblCompany = new JLabel("Company");
+        lblCompany.setFont(new Font("", 1, 20));
+        lblPannel.add(lblCompany);
+
+        lblSalary = new JLabel("Salary");
+        lblSalary.setFont(new Font("", 1, 20));
+        lblPannel.add(lblSalary);
+
+        lblBirthday = new JLabel("Birthday");
+        lblBirthday.setFont(new Font("", 1, 20));
+        lblPannel.add(lblBirthday);
+
+        add("West", lblPannel);
+
+        JPanel textPannel = new JPanel(new GridLayout(6, 1));
+        String newID = AddContactForm.displayID(count);
+        JLabel TETid = new JLabel(newID);
+        id = new JTextField(newID);
+        TETid.setFont(new Font("", 1, 20));
+        // JPanel idTextJPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        // idTextJPanel.add(TETid);
+        textPannel.add(TETid);
+
+        textName = new JTextField(10);
+        textName.setFont(new Font("", 1, 20));
+        JPanel nameTextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        nameTextPannel.add(textName);
+        textPannel.add(nameTextPannel);
+
+        textContactNumber = new JTextField(10);
+        textContactNumber.setFont(new Font("", 1, 20));
+        JPanel contactTextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        contactTextPannel.add(textContactNumber);
+        textPannel.add(contactTextPannel);
+
+        textCompany = new JTextField(10);
+        textCompany.setFont(new Font("", 1, 20));
+        JPanel companyTextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        companyTextPannel.add(textCompany);
+        textPannel.add(companyTextPannel);
+
+        textSalary = new JTextField(6);
+        textSalary.setFont(new Font("", 1, 20));
+        JPanel salaryTextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        salaryTextPannel.add(textSalary);
+        textPannel.add(salaryTextPannel);
+
+        textBirthday = new JTextField(6);
+        textBirthday.setFont(new Font("", 1, 20));
+        JPanel birthdayTextPannel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        birthdayTextPannel.add(textBirthday);
+        textPannel.add(birthdayTextPannel);
+
+        // lblandtextPannel.add(lblPannel);
+        // lblandtextPannel.add(textPannel);
+
+        add("Center", textPannel);
+
+    }
+}
+
+class ViewContactForm extends JFrame {
+    private JTable ContactTable;
+    private DefaultTableModel dtm;
+
+    private JLabel tiLabel;
+
+    private JButton backhome;
+
+    ViewContactForm() {
+        setSize(400, 300);
+        setTitle("View Contact");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        tiLabel = new JLabel("List Contact By Name");
+        tiLabel.setHorizontalAlignment(JLabel.CENTER);
+        tiLabel.setFont(new Font("", 1, 30));
+
+        add("North", tiLabel);
+
+        String[] columnName = { "Contact ID", "Name", "Contact Number", "Company", "Salary", "Birthday" };
+        dtm = new DefaultTableModel(columnName, 0);
+
+        ContactTable = new JTable(dtm);
+
+        JScrollPane tablepane = new JScrollPane(ContactTable);
+
+        add("Center", tablepane);
+
+        JPanel btnPannel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        backhome = new JButton("Back to Home");
+        backhome.setFont(new Font("", 1, 20));
+        btnPannel.add(backhome);
+
+        add("South", btnPannel);
+
+    }
+}
+
+class ContactHomeForm extends JFrame {
+    static int count = 0;
+    public static ArrayList<Contact> contactList = new ArrayList<>();
+
+    private AddContactForm addcontactform;
+    private ViewContactForm viewcontactform;
+
+    private JButton btnAddContact;
+    private JButton btnUpdateContact;
+    private JButton btnSearchContact;
+    private JButton btnDeleteContact;
+    private JButton btnViewContact;
+    private JButton btnExit;
+
+    ContactHomeForm() {
+        setSize(500, 300);
+        setTitle("Contact management System");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel btnPannel = new JPanel(new GridLayout(6, 1));
+
+        btnAddContact = new JButton("Add New Contact");
+        btnAddContact.setFont(new Font("", 1, 25));
+        btnAddContact.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (addcontactform == null) {
+                    addcontactform = new AddContactForm(count++);
+                }
+                addcontactform.setVisible(true);
+            }
+        });
+        btnPannel.add(btnAddContact);
+
+        btnUpdateContact = new JButton("Update Contact");
+        btnUpdateContact.setFont(new Font("", 1, 25));
+        btnPannel.add(btnUpdateContact);
+
+        btnSearchContact = new JButton("Search Contact");
+        btnSearchContact.setFont(new Font("", 1, 25));
+        btnPannel.add(btnSearchContact);
+
+        btnDeleteContact = new JButton("Delete Contact");
+        btnDeleteContact.setFont(new Font("", 1, 25));
+        btnPannel.add(btnDeleteContact);
+
+        btnViewContact = new JButton("View Contact");
+        btnViewContact.setFont(new Font("", 1, 25));
+        btnPannel.add(btnViewContact);
+
+        JPanel exitPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        btnExit = new JButton("Exit");
+        btnExit.setFont(new Font("", 1, 25));
+        exitPanel.add(btnExit);
+
+        btnPannel.add(exitPanel);
+
+        add("Center", btnPannel);
+    }
 }
 
 class Ifrend {
-    public static void main(String[] args) {
-        ContactHomeForm contactHomeForm = new ContactHomeForm();
-        contactHomeForm.setVisible(true);
+    public static void main(String args[]) {
+        // new AddContactForm().setVisible(true);
+        // new ViewContactForm().setVisible(true);
+        new ContactHomeForm().setVisible(true);
     }
 }
